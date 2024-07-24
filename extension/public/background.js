dfe80c25ff9a37bd365fd459ee101ff459754d0e
jrafaaael <jrmd070102@gmail.com>
2024-07-24 19:19:14 -0400

    feat(ext): use side panel

---
+++ extension/public/background.js
@@ -0,0 +1,4 @@
+// Allows users to open the side panel by clicking on the action toolbar icon
+chrome.sidePanel
+  .setPanelBehavior({ openPanelOnActionClick: true })
+  .catch((error) => console.error(error));
