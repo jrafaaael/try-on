b6178186b73ea62355e0f9d67271222628862a8a
jrafaaael <jrmd070102@gmail.com>
2024-07-24 19:46:11 -0400

    feat(ext): add `Geist` variable font

--- extension/tailwind.config.js
+++ extension/tailwind.config.js
@@ -1,6 +1,14 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
-  theme: {},
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Geist", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
   plugins: [],
 };
