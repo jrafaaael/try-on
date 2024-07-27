a71af7181a3be297eef4be4c1f662079ff6f0d3b
jrafaaael <jrmd070102@gmail.com>
2024-07-27 10:28:32 -0400

    feat(ext): allow drag styles

--- extension/src/components/DnD.vue
+++ extension/src/components/DnD.vue
@@ -1,8 +1,12 @@
 <script setup lang="ts">
 import { ref } from "vue";
 
-defineProps({ accept: String });
+defineProps<{
+  accept: string;
+  classes?: { default?: string; dragging?: string };
+}>();
 const inputRef = ref<HTMLInputElement>();
+const isDragging = ref(false);
 const emit = defineEmits(["change"]);
 
 function handleUpload(e: Event) {
@@ -17,6 +21,8 @@ function handleUpload(e: Event) {
 function handleDrop(e: Event) {
   const dt = e.dataTransfer;
   const files = dt?.files;
+
+  isDragging.value = false;
 
   if (!files) return;
 
@@ -35,16 +41,26 @@ function handleDrop(e: Event) {
     @change="handleUpload"
   />
   <button
-    :class="$attrs.class"
+    class="dropzone"
+    :class="[
+      $attrs.class ?? $props.classes?.default,
+      isDragging ? $props.classes?.dragging : '',
+    ]"
     @click="() => inputRef && inputRef.click()"
     @drag.prevent.stop=""
     @dragstart.prevent.stop=""
     @dragend.prevent.stop=""
-    @dragenter.prevent.stop=""
+    @dragenter.prevent.stop="isDragging = true"
     @dragover.prevent.stop=""
-    @dragleave.prevent.stop=""
+    @dragleave.prevent.stop="isDragging = false"
     @drop.prevent.stop="handleDrop"
   >
     <slot></slot>
   </button>
 </template>
+
+<style>
+.dropzone * {
+  pointer-events: none;
+}
+</style>
--- extension/src/components/Model.vue
+++ extension/src/components/Model.vue
@@ -19,7 +19,8 @@ function handleChange(data) {
     <h2 class="mb-2 text-2xl font-black">Your model photo 👤</h2>
     <DnD
       accept="image/*"
-      class="w-full h-64 p-4 bg-neutral-100 border border-neutral-200 text-neutral-600 rounded-lg flex flex-col justify-center items-center gap-2"
+      class="w-full h-64 p-4 bg-neutral-100 border-2 border-neutral-200 text-neutral-600 rounded-lg flex flex-col justify-center items-center gap-2"
+      :classes="{ dragging: 'border-2 border-dashed border-blue-400' }"
       @change="handleChange"
     >
       <template v-if="model === null">
