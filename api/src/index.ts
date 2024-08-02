c94a2b3da4fb2fa5c437598b26b8d57e5613d59a
jrafaaael <jrmd070102@gmail.com>
2024-08-01 20:04:32 -0400

    fix(api): cors

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,7 +1,14 @@
 import { Hono } from 'hono';
+import { cors } from 'hono/cors';
 import validate from './routes/validate.route';
 
 const app = new Hono().basePath('/api');
+
+app.use(
+	cors({
+		origin: '*',
+	})
+);
 
 app.route('/validate', validate);
 
