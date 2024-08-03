2475c994668166293a172ec804985367a6ed0641
jrafaaael <jrmd070102@gmail.com>
2024-08-03 19:23:13 -0400

    fix(api): improve zod schema description

--- api/src/schemas/validate.schema.ts
+++ api/src/schemas/validate.schema.ts
@@ -1,12 +1,16 @@
 import { z } from 'zod';
 
 export const validateModelSchema = z.object({
-	isHuman: z.boolean(),
-	isShowingUpperBody: z.boolean().describe('boolean that indicates if the human on the image is showing their upper-body'),
+	isHuman: z.boolean().describe('boolean that indicates if the model is a human'),
+	isShowingUpperBody: z
+		.boolean()
+		.describe(
+			'boolean that indicates if the human on the image is showing their full upper-body. LOWER-BODY ONLY IS NOT VALID. HALF UPPER-BODY IS NOT VALID'
+		),
 });
 
 export const validateGarmentSchema = z.object({
 	isCloth: z.boolean().describe('boolean that indicates if the image has a valid garment'),
 	isUpperBody: z.boolean().describe('boolean that indicates if the garment is upper-body garment'),
-	hasHuman: z.boolean().describe('booleant that indicates if image has a model wearing the garment'),
+	hasHuman: z.boolean().describe('boolean that indicates if image has a model wearing the garment'),
 });
