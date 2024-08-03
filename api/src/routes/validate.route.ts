522b50b9ba7067d6067878c7aa362f988b679f33
jrafaaael <jrmd070102@gmail.com>
2024-08-03 08:54:42 -0400

    feat(api): add route to validate `garment`

--- api/src/routes/validate.route.ts
+++ api/src/routes/validate.route.ts
@@ -1,7 +1,7 @@
 import { Hono } from 'hono';
 import { generateObject } from 'ai';
 import { createGoogleGenerativeAI } from '@ai-sdk/google';
-import { validateModelSchema } from '../schemas/validate.schema';
+import { validateGarmentSchema, validateModelSchema } from '../schemas/validate.schema';
 
 const app = new Hono();
 
@@ -33,6 +33,44 @@ app.post('/model', async (c) => {
 
 		if (!object.isHuman) return c.json({ errors: [{ message: 'Model should be an human' }] }, 400);
 		if (!object.isShowingUpperBody) return c.json({ errors: [{ message: 'Model should show at least their upper-body' }] }, 400);
+
+		return c.json({ data: object }, 200);
+	} catch (error) {
+		console.log(error);
+
+		return c.json({ errors: [{ message: 'Something went wrong' }] }, 500);
+	}
+});
+
+app.post('/garment', async (c) => {
+	const body = await c.req.parseBody();
+	const garment = body['garment'] as File;
+	const google = createGoogleGenerativeAI({
+		apiKey: c.env['GOOGLE_GENERATIVE_AI_API_KEY'],
+	});
+
+	try {
+		const { object } = await generateObject({
+			model: google('models/gemini-1.5-flash-latest'),
+			schema: validateGarmentSchema,
+			system: `
+				You validate images with REAL UPPER-BODY garment on it.
+				A valid garment image CAN'T have HUMAN wearing the garment.
+			`.trim(),
+			messages: [
+				{
+					role: 'user',
+					content: [{ type: 'image', image: await garment.arrayBuffer() }],
+				},
+			],
+			temperature: 0,
+			maxRetries: 0,
+			mode: 'tool',
+		});
+
+		if (!object.isCloth) return c.json({ errors: [{ message: 'Image must be a valid cloth' }] }, 400);
+		if (!object.isUpperBody) return c.json({ errors: [{ message: 'Garment must be upper-body' }] }, 400);
+		if (object.hasHuman) return c.json({ errors: [{ message: 'Garment image must not have a model' }] }, 400);
 
 		return c.json({ data: object }, 200);
 	} catch (error) {
--- api/src/schemas/validate.schema.ts
+++ api/src/schemas/validate.schema.ts
@@ -4,3 +4,9 @@ export const validateModelSchema = z.object({
 	isHuman: z.boolean(),
 	isShowingUpperBody: z.boolean().describe('boolean that indicates if the human on the image is showing their upper-body'),
 });
+
+export const validateGarmentSchema = z.object({
+	isCloth: z.boolean().describe('boolean that indicates if the image has a valid garment'),
+	isUpperBody: z.boolean().describe('boolean that indicates if the garment is upper-body garment'),
+	hasHuman: z.boolean().describe('booleant that indicates if image has a model wearing the garment'),
+});
